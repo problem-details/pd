1f79d0f6a7d1ddb2e5f1d94a56ed01d7a5f8ae8b
SinaMobasheri <me@sinamobasheri.ir>
2024-05-29 13:25:44 +0330

    Add `ProblemDetails` structure
    Add `ProblemDetails` structure representing "Problem Details JSON Object" as
    described in RFC 7807 and implementing `error` interface for it.

--- pd/pd.go
+++ pd/pd.go
@@ -1,2 +1,27 @@
 // https://www.rfc-editor.org/rfc/rfc7807.txt
 package pd
+
+import (
+	"encoding/json"
+)
+
+type ProblemDetails struct {
+	Type     string `json:"type,omitempty"`
+	Title    string `json:"title,omitempty"`
+	Status   int    `json:"status,omitempty"`
+	Detail   string `json:"detail,omitempty"`
+	Instance string `json:"instance,omitempty"`
+	Err      error  `json:"-"`
+}
+
+func (details *ProblemDetails) Error() string {
+	bs, err := json.Marshal(details)
+	if err != nil {
+		panic(err) // Is it possible to panic?
+	}
+	return string(bs)
+}
+
+func (details *ProblemDetails) Unwrap() error {
+	return details.Err
+}
